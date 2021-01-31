992455f534011e9f2d4af1d0ebcbe8d6974de009
Unity05 <friedward.wenzler@outlook.de>
2021-01-31 01:44:50 +0100

    Added la place smoothing to Bernoulli Naive Bayes.

--- Generative_Learning_Algorithms/Naive_Bayes/BernoulliNaiveBayes.py
+++ Generative_Learning_Algorithms/Naive_Bayes/BernoulliNaiveBayes.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 
 class BernoulliNaiveBayes:
-    def __init__(self, samples: pd.DataFrame):
+    def __init__(self, samples: pd.DataFrame, la_place_smoothing=True):
         self.vocab_size = samples['sample_vectors'].loc[0].size
         self.samples_len = len(samples)
         self.phi = (samples.loc[samples['labels'] == 1].sum().loc['labels'] / self.samples_len)
-        self.phi_word_class = self.__calculate_estimates(samples=samples)
+        self.phi_word_class = self.__calculate_estimates(samples=samples, la_place_smoothing=la_place_smoothing)
 
     def get_class(self, input_vector: np.array):
         return np.argmax([
@@ -20,12 +20,15 @@ class BernoulliNaiveBayes:
         p = self.__p_x_given_y(input_vector=input_vector, label=label) * phi
         return p / (p + (self.__p_x_given_y(input_vector=input_vector, label=(1 - label)) * (1 - phi)))
 
-    def __calculate_estimates(self, samples: pd.DataFrame):
+    def __calculate_estimates(self, samples: pd.DataFrame, la_place_smoothing: bool):
         samples_zero = samples.loc[samples['labels'] == 0]
         samples_one = samples.loc[samples['labels'] == 1]
+        la_place_smoothing_x = int(la_place_smoothing)
         return np.array([
-            (samples_zero['sample_vectors'].sum() / len(samples_zero)),
-            (samples_one['sample_vectors'].sum() / len(samples_one))
+            ((samples_zero['sample_vectors'].sum() + la_place_smoothing_x)
+             / (len(samples_zero) + (la_place_smoothing_x * 2))),
+            ((samples_one['sample_vectors'].sum() + la_place_smoothing_x)
+             / (len(samples_one) + (la_place_smoothing_x * 2)))
         ])
 
     def __p_x_given_y(self, input_vector: np.array, label: int):
