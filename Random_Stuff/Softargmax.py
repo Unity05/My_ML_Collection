11c0fc90b8462937b77f86315298eacce5529ede
Unity05 <friedward.wenzler@outlook.de>
2021-04-02 00:08:16 +0200

    Updated Softargmax.

--- Random_Stuff/Softargmax.py
+++ Random_Stuff/Softargmax.py
@@ -2,5 +2,7 @@ import numpy as np
 
 
 def softargmax(values: np.ndarray, beta: float) -> np.ndarray:
-    return np.sum(np.exp(beta * values) / np.sum(np.exp(beta * values)) * values)
-
+    return np.matmul(
+        np.exp(beta * values) / np.sum(np.exp(beta * values)),
+        np.transpose(np.arange(values.shape[0] - 1))
+    )
