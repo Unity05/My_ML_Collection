8fcc4b4e9c9f14d19cd100484a671f4773362d71
Unity05 <friedward.wenzler@outlook.de>
2021-09-15 15:23:11 +0200

    Added modularity loss.

--- Graph_Net/Loss.py
+++ Graph_Net/Loss.py
@@ -1,4 +1,7 @@
+import torch
 import numpy as np
+
+from collections import defaultdict
 
 from Graph_Net.RandomWalks import RandomWalk
 from Random_Stuff.Activations import sigmoid
@@ -41,3 +44,38 @@ class RandomWalkNeighbourLoss:
                 np.arange(self.num_nodes),
                 p=self.node_p_distribution
             )))
+
+
+class Modularity:
+    @staticmethod
+    def modularity(edge_index: torch.Tensor, node_degrees: torch.Tensor, community_index: torch.Tensor):
+        edge_index.t_()
+        m = edge_index.shape[0]
+        offset = 0
+        modularity = 0
+        edge_counter = defaultdict(bool)
+        for edge_i in range(m):
+            edge = edge_index[edge_i]
+            n_expected_edges = (node_degrees[edge[0]] * node_degrees[edge[1]]) / (2 * m)
+            if not edge_counter[tuple(edge.tolist())]:
+                edge_counter[tuple(edge.tolist())] = True
+                offset += n_expected_edges
+                modularity += (1 - n_expected_edges)
+            else:
+                modularity += 1
+
+        n_cummunities = torch.unique(community_index).shape[0]
+        community_sums = torch.zeros(size=(n_cummunities, ))
+        for degree, community_i in zip(node_degrees, community_index):
+            community_sums[community_i.item()] += degree
+
+        for degree, community_i in zip(node_degrees, community_index):
+            p = degree / (2 * m)
+            for community_j in range(n_cummunities):
+                if community_j == community_i:
+                    continue
+                modularity -= p * community_sums[community_j]
+
+        modularity = (1 / (2 * m)) * (modularity + offset)
+
+        return modularity
